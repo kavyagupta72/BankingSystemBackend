ca5e4597ac8e2573dcb9b51fdc5462c16f58c115
Kavya Gupta <kavyaaaaaa72@gmail.com>
2026-06-19 22:03:08 +0530

    finish user register controller(basically user api)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,31 @@
 const userModel = require("../models/user,model")
+const jwt = require("jsonwebtoken")
 
+//POST api/auth/register
 function userRegisterController(req,res){
-    
+    const {name,email,password} = req.body
+    const isExists = userModel.findOne({
+        email : email
+    })
+    if(isExists){
+        return res.status(422).json({
+            message : "User already registered with this email",
+            status : "failed"
+        })
+    }
+    const user = await userModel.create({
+        email,password,mail
+    })
+    const token = jwt.sign({payload : user._id}, process.env.JWT_SECRET,{expiresIn : "2d"})
+    res.cookies("token",token)
+    res.status(201).json({      //whenever a new resource is created, we use 201 status code
+        user : {
+            _id : user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    })
+
 }
+module.exports = {UserRegisterController}
